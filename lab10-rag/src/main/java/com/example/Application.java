bf83a71620c2ccf7d862dd1e777e08f3f899b246
amresh-das <mail2amreshdas@gmail.com>
2025-03-28 09:16:22 +0530

    lab10 complete

--- lab10-rag/src/main/java/com/example/Application.java
+++ lab10-rag/src/main/java/com/example/Application.java
@@ -1,7 +1,12 @@
 package com.example;
 
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 
 @SpringBootApplication
@@ -11,10 +16,14 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	//	TODO-04: Define a bean method named "vectorStore" of type VectorStore.
+	//	TDODO-04: Define a bean method named "vectorStore" of type VectorStore.
 	//  The method should accept an EmbeddingModel parameter.
 	//	Have it instantiate and return a new SimpleVectorStore injected with the given EmbeddingModel.
 	//  Use @Profile to assign this bean to the "simple-vector-store" profile.
 
-	
+	@Bean
+	@Profile("simple-vector-store")
+	public VectorStore vectorStore(final EmbeddingModel embeddingModel) {
+		return SimpleVectorStore.builder(embeddingModel).build();
+	}
 }
--- lab10-rag/src/test/java/com/example/client/AIClientImplTests.java
+++ lab10-rag/src/test/java/com/example/client/AIClientImplTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//  TODO-11: Define this test class as a Spring Boot test.
+//  TDODO-11: Define this test class as a Spring Boot test.
 //  Use the @ActiveProfiles annotation to activate: 
 //  1) the "simple-vector-store" profile  AND
 //  2) the matching chat model you plan to use:
@@ -17,26 +17,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 //      For Ollama,             use "ollama".
 
 
-
+@SpringBootTest
 // @ActiveProfiles({"simple-vector-store","aws"})
 // @ActiveProfiles({"simple-vector-store","azure"})
-// @ActiveProfiles({"simple-vector-store","openai"})
+ @ActiveProfiles({"simple-vector-store","openai"})
 // @ActiveProfiles({"simple-vector-store","ollama"})
 public class AIClientImplTests {
 
-    //  TODO-12: Use the @Autowired annotation to inject an instance of the AIClient.
+    //  TDODO-12: Use the @Autowired annotation to inject an instance of the AIClient.
+    @Autowired private AIClient client;
 
-
-    //  TODO-13: Write a @Test method to validate the AIClient.
+    //  TDODO-13: Write a @Test method to validate the AIClient.
     //  First, call the client's save() method with the Utilities.productCatalog list; this populates the test data.
     //  Next, call the client's getProductRecommendations() method with the Utilities.query String.
     //  Use AssertJ's Assertions.assertThat() method to ensure that the content is not null.
     //  Use AssertJ's Assertions.assertThat() method to ensure that the content contains expected results.
     //  Utilities.sampleResults array contains the expected results.
     //  Print the response string that is returned.
+    @Test
+    public void testGetProductRecommendations() {
+        client.save(Utilities.productCatalog);
+        String response = client.getProductRecommendations(Utilities.query);
+        System.out.println(response);
 
+        assertThat(response).isNotNull();
+        assertThat(response).contains(Utilities.sampleResults);
+    }
 
-    //  TODO-14: Save all work.  Run this test, it should pass.
+    //  TDODO-14: Save all work.  Run this test, it should pass.
 
 
 
